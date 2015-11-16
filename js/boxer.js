2a4b68cf9846f6bc99a6a9c49877aa54ed74d530
John Hearn <jhearn@gmail.com>
2015-11-16 12:22:27 -0800

    added a image file for a dot tile, converted floor tile to PNG format, and added basic functionality to update gameboard tiles

--- js/boxer.js
+++ js/boxer.js
@@ -6,6 +6,20 @@
  * CF201       Fall 2015       *
  * * * * * * * * * * * * * * * */
 
+// all level data: this will eventually be moved to a JSON file
+var levelData = [ { floor: [ "0101", "0201", "0301", "0102", "0202", "0302",
+			     "0103", "0203", "0303", "0703", "0304", "0704",
+			     "0305", "0405", "0505", "0605", "0705", "0206",
+			     "0306", "0406", "0606", "0706", "0207", "0307",
+			     "0407" ],
+		    start: [ "0101" ],
+		    crate: [ "0202", "0302", "0203" ],
+		    dots:  [ "0703", "0704", "0705" ] } ]
+
+var brickURL = "img/RedBrick.png";
+var floorURL = "img/FloorTile.jpg";
+var crateURL = "img/WoodenCrate.png";
+
 // We want our coordinates to be 4-digit strings, so
 // we need to pad single digits with a leading zero.
 var pad = function (num, size) {
@@ -19,8 +33,7 @@ function GameBoard(){
     this.element.setAttribute( "id", "container" );
     for ( var ii = 0; ii < 20; ii++ ) {
 	for ( var jj = 0; jj < 20; jj++ ) {
-	    var coordinate = pad(ii, 2) + pad(jj, 2);
-	    console.log(coordinate);
+	    var coordinate = pad(jj, 2) + pad(ii, 2);
 	    var cell = document.createElement( "div" );
 	    cell.setAttribute( "data-coord", coordinate );
 	    var tile = document.createElement("img");
@@ -39,6 +52,33 @@ var BOXER_GAME_MODULE = (function() {
     my.game = new GameBoard();
     my.anchorNode.appendChild( my.game.element );
 
-    //var $crate = $('div').find(el+[data-coord=+current+]);
+    my.updateCell = function(coord, tileURL) {
+	// Pretty sure this is a bad way to do this. Probably bad performance.
+	var cell = document.querySelector("[data-coord='" + coord + "']");
+	// This also seems not great.
+	var child = cell.children[0];
+	child.src = tileURL;
+    }
+    
+    my.loadLevel = function( levelObject ) {
+	// update floor tiles
+	for ( var ii = 0; ii < levelObject.floor.length; ii++ ) {
+	    my.updateCell(levelObject.floor[ii], floorURL );
+	}
+	// update dot tiles
+	for ( var ii = 0; ii < levelObject.dots.length; ii++ ) {
+	    my.updateCell(levelObject.dots[ii], dotsURL );
+	}
+	
+	// update crate tiles
+	for ( var ii = 0; ii < levelObject.crate.length; ii++ ) {
+	    my.updateCell(levelObject.crate[ii], crateURL );
+	}
+    }
+    
+
+    my.loadLevel( levelData[0] );
+    
+
     return my;
 })();
