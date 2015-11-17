550834bb8f7cb5ebe480046ddf5b3c730d7db6cf
John Hearn <jhearn@gmail.com>
2015-11-16 16:02:37 -0800

    converted DOM manipulation to JQuery and refactored gameboard to store references to cell divs in a 2 dimensional array for easy access (and improved performance)

--- js/boxer.js
+++ js/boxer.js
@@ -7,18 +7,22 @@
  * * * * * * * * * * * * * * * */
 
 // all level data: this will eventually be moved to a JSON file
-var levelData = [ { floor: [ "0101", "0201", "0301", "0102", "0202", "0302",
-			     "0103", "0203", "0303", "0703", "0304", "0704",
-			     "0305", "0405", "0505", "0605", "0705", "0206",
-			     "0306", "0406", "0606", "0706", "0207", "0307",
-			     "0407" ],
-		    start: [ "0101" ],
-		    crate: [ "0202", "0302", "0203" ],
-		    dots:  [ "0703", "0704", "0705" ] } ]
+var levelData = [ { floor: [ [ 01, 01 ], [ 02, 01 ], [ 03, 01 ], [ 01, 02 ],
+			     [ 02, 02 ], [ 03, 02 ], [ 01, 03 ], [ 02, 03 ],
+			     [ 03, 03 ], [ 07, 03 ], [ 03, 04 ], [ 07, 04 ],
+			     [ 03, 05 ], [ 04, 05 ], [ 05, 05 ], [ 06, 05 ],
+			     [ 07, 05 ], [ 02, 06 ], [ 03, 06 ], [ 04, 06 ],
+			     [ 06, 06 ], [ 07, 06 ], [ 02, 07 ], [ 03, 07 ],
+			     [ 04, 07 ] ],
+		    start: [ 01, 01 ],
+		    crate: [ [ 02, 02 ], [ 03, 02 ], [ 02, 03 ] ],
+		    dots:  [ [ 07, 03 ], [ 07, 04 ], [ 07, 05 ] ]
+		  } ]
 
-var brickURL = "img/RedBrick.png";
-var floorURL = "img/FloorTile.jpg";
+var wallURL = "img/RedBrick.png";
+var floorURL = "img/FloorTile.png";
 var crateURL = "img/WoodenCrate.png";
+var dotsURL  = "img/DotTile.png";
 
 // We want our coordinates to be 4-digit strings, so
 // we need to pad single digits with a leading zero.
@@ -28,19 +32,24 @@ var pad = function (num, size) {
     return s;
 }
 
-function GameBoard(){
-    this.element = document.createElement( "section" );
-    this.element.setAttribute( "id", "container" );
+function Coord(tileType, tileURL) {
+    this.$div = $( '<div></div>' );
+    this.$img = $( '<img></img>' );
+    this.tile = tileType;
+    this.$img.attr( 'src', tileURL );
+    this.$div.append( this.$img );
+}
+
+function GameBoard() {
+    this.coordinates = [ ];
+    this.$element = $('<section></section>').attr( 'id', "container" );
     for ( var ii = 0; ii < 20; ii++ ) {
 	for ( var jj = 0; jj < 20; jj++ ) {
-	    var coordinate = pad(jj, 2) + pad(ii, 2);
-	    var cell = document.createElement( "div" );
-	    cell.setAttribute( "data-coord", coordinate );
-	    var tile = document.createElement("img");
-	    tile.src = "img/RedBrick.png";
-	    cell.appendChild( tile );
-	    this.element.appendChild( cell );
+	    this.coordinates.push( [ ] );
+	    this.coordinates[jj].push( new Coord( "wall", wallURL ) );
+	    this.$element.append( this.coordinates[jj][ii].$div );
 	}
+	
     }
 }
 
@@ -48,37 +57,34 @@ function GameBoard(){
 var BOXER_GAME_MODULE = (function() {
     
     var my = {};
-    my.anchorNode = document.getElementById( "gameBoard" );
+    my.$anchor = $( "#gameBoard" );
     my.game = new GameBoard();
-    my.anchorNode.appendChild( my.game.element );
+    my.$anchor.append( my.game.$element );
 
-    my.updateCell = function(coord, tileURL) {
-	// Pretty sure this is a bad way to do this. Probably bad performance.
-	var cell = document.querySelector("[data-coord='" + coord + "']");
-	// This also seems not great.
-	var child = cell.children[0];
-	child.src = tileURL;
+    my.updateCell = function( [x, y], tileType, tileURL) {
+	my.game.coordinates[x][y].tile = tileType;
+	my.game.coordinates[x][y].$img.attr( 'src', tileURL );
+
     }
     
     my.loadLevel = function( levelObject ) {
 	// update floor tiles
 	for ( var ii = 0; ii < levelObject.floor.length; ii++ ) {
-	    my.updateCell(levelObject.floor[ii], floorURL );
+	    my.updateCell(levelObject.floor[ii], "floor", floorURL );
 	}
 	// update dot tiles
 	for ( var ii = 0; ii < levelObject.dots.length; ii++ ) {
-	    my.updateCell(levelObject.dots[ii], dotsURL );
+	    my.updateCell(levelObject.dots[ii], "dots", dotsURL );
 	}
 	
 	// update crate tiles
 	for ( var ii = 0; ii < levelObject.crate.length; ii++ ) {
-	    my.updateCell(levelObject.crate[ii], crateURL );
+	    my.updateCell(levelObject.crate[ii], "crate", crateURL );
 	}
     }
     
 
     my.loadLevel( levelData[0] );
-    
 
     return my;
 })();
